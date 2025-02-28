ab14e41991913b252d50db1fdc2c3f3f64e95387
aashishrewa2001 <aashishrewa2001@gmail.com>
2025-03-01 00:36:49 +0530

    cors security have been removed

--- backend/index.js
+++ backend/index.js
@@ -37,12 +37,12 @@ app.use(cors({
     credentials: true,
 }));
 
-// Global headers for security & CORS handling
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-});
+// // Global headers for security & CORS handling
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     next();
+// });
 
 // Connect to Database
 connectDB();
